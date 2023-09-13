607e6e33a5715fa477d3a8b055f110f4e14e8186
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-13 20:49:01 -0300

    fix: skeleton

--- kz-feed/src/components/loginForm/index.tsx
+++ kz-feed/src/components/loginForm/index.tsx
@@ -7,17 +7,19 @@ export const LoginForm = () => {
     return(
         <> 
             <section>
-                <TitleTwo>Acesse ao KZFeed</TitleTwo>
-                <ParagraphOne>Preencha os campos corretamente para fazer login</ParagraphOne>
-            </section>
-            <form>
-                <Input inputsize = "medium" placeholder="E-mail" type="email"/>
-                <Input inputsize = "medium" placeholder="Senha" type="password"/>
-                <Button buttonsize="medium" buttonstyle="noneoutline">Entrar</Button> 
-            </form>
-            <section>
-                <ParagraphOne>Não é cadastrado?</ParagraphOne>
-                <Link to = "/register">Cadastre-se</Link>
+                <section>
+                    <TitleTwo>Acesse ao KZFeed</TitleTwo>
+                    <ParagraphOne>Preencha os campos corretamente para fazer login</ParagraphOne>
+                </section>
+                <form>
+                    <Input inputsize = "medium" placeholder="E-mail" type="email"/>
+                    <Input inputsize = "medium" placeholder="Senha" type="password"/>
+                    <Button buttonsize="medium" buttonstyle="noneoutline">Entrar</Button> 
+                </form>
+                <section>
+                    <ParagraphOne>Não é cadastrado?</ParagraphOne>
+                    <Link to = "/register">Cadastre-se</Link>
+                </section>
             </section>
         </>
     )
