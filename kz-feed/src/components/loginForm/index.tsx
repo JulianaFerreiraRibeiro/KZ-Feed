0aec00bd15bf2c65fdab5ceeb623394a9c66af8d
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-13 20:46:40 -0300

    Feat/LoginForm skeleton

--- kz-feed/src/components/loginForm/index.tsx
+++ kz-feed/src/components/loginForm/index.tsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom"
+import { Button } from "../button"
 import { Input } from "../input"
+import { ParagraphOne, TitleTwo } from "../typography"
 
 export const LoginForm = () => {
     return(
-        <form>
-            <Input inputsize = "medium" placeholder="E-mail" type="email"/>
-            <Input inputsize = "medium" placeholder="Senha" type="password"/>
-        </form>
+        <> 
+            <section>
+                <TitleTwo>Acesse ao KZFeed</TitleTwo>
+                <ParagraphOne>Preencha os campos corretamente para fazer login</ParagraphOne>
+            </section>
+            <form>
+                <Input inputsize = "medium" placeholder="E-mail" type="email"/>
+                <Input inputsize = "medium" placeholder="Senha" type="password"/>
+                <Button buttonsize="medium" buttonstyle="noneoutline">Entrar</Button> 
+            </form>
+            <section>
+                <ParagraphOne>Não é cadastrado?</ParagraphOne>
+                <Link to = "/register">Cadastre-se</Link>
+            </section>
+        </>
     )
 }
