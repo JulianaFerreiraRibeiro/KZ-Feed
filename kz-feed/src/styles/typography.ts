c83124383d257574eeea28902c284066f1228238
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-12 21:56:14 -0300

    feat/Typography created styles

---
+++ kz-feed/src/styles/typography.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledTitleOne = styled.h1`
+    font-size: var(--font-size-1);
+    font-weight: var(--font-weight-1);
+`
+
+export const StyledTitleTwo = styled.h2`
+    font-size: var(--font-size-2);
+    font-weight: var(--font-weight-1);
+`
+
+export const StyledTitleThree = styled.h3`
+    font-size: var(--font-size-3);
+    font-weight: var(--font-weight-1);
+`
+
+export const StyledTitleFour = styled.h4`
+    font-size: var(--font-size-4);
+    font-weight: var(--font-weight-1);
+`
+
+export const StyledParagraphOne = styled.p`
+    font-size: var(--font-size-5);
+    font-weight: var(--font-weight-2);
+`
+
+export const StyledParagraphTwo = styled.p`
+    font-size: var(--font-size-6);
+    font-weight: var(--font-weight-2);
+`
