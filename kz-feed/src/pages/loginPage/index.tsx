b0def51485746d18ac60e6b6f057269f34341f0b
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-12 21:11:10 -0300

    feat/LoginPage skeleton

--- kz-feed/src/pages/loginPage/index.tsx
+++ kz-feed/src/pages/loginPage/index.tsx
@@ -1,7 +1,11 @@
+import { LoginForm } from "../../components/loginForm"
+import loginBanner from "../../assets/login_banner.svg"
+
 export const LoginPage = () => {
     return(
         <section>
-            
+            <img src={loginBanner} alt = "imagem de um cenário branco em que há um caderno pequeno com um lápis preto em cima, ao lado há um notebook e uma caneca branca" />
+            <LoginForm/>
         </section>
     )
 }
