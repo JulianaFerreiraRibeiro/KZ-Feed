26fa60acce384f7eb1a2f3646d16ff536e3ce439
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-13 21:27:06 -0300

    Feat/LoginForm hookform applied

--- kz-feed/src/components/input/index.tsx
+++ kz-feed/src/components/input/index.tsx
@@ -1,13 +1,15 @@
+import { UseFormRegisterReturn } from "react-hook-form";
 import { StyledInput } from "../../styles/input";
 
 interface IInputProps {
     placeholder: string,
     type: string;
     inputsize: "medium" | "big";
+    register: UseFormRegisterReturn<string>;
 }
 
-export const Input = ({placeholder, type, inputsize}: IInputProps) => {
+export const Input = ({placeholder, type, inputsize, register}: IInputProps) => {
     return( 
-        <StyledInput inputsize = {inputsize} placeholder={placeholder} type = {type}/>
+        <StyledInput inputsize = {inputsize} placeholder={placeholder} type = {type} {...register}/>
     )
 }
--- kz-feed/src/components/loginForm/index.tsx
+++ kz-feed/src/components/loginForm/index.tsx
@@ -3,8 +3,20 @@ import { Button } from "../button"
 import { Input } from "../input"
 import { ParagraphOne, TitleTwo } from "../typography"
 import { StyledLoginFormContainer } from "./style"
+import { useForm } from "react-hook-form"
+
+interface ILoginFormData{
+    email: string;
+    password: string;
+}
 
 export const LoginForm = () => {
+    const {handleSubmit, register} = useForm<ILoginFormData>()
+
+    const submit = async (formData: ILoginFormData): Promise<void> => {
+        console.log(formData)
+    }
+
     return(
         <> 
             <StyledLoginFormContainer>
@@ -12,9 +24,9 @@ export const LoginForm = () => {
                     <TitleTwo>Acesse o KZFeed</TitleTwo>
                     <ParagraphOne>Preencha os campos corretamente para fazer login</ParagraphOne>
                 </section>
-                <form className="loginForm">
-                    <Input inputsize = "medium" placeholder="E-mail" type="email"/>
-                    <Input inputsize = "medium" placeholder="Senha" type="password"/>
+                <form className="loginForm" onSubmit={handleSubmit(submit)}>
+                    <Input inputsize = "medium" placeholder="E-mail" type="email" register={register("email")}/>
+                    <Input inputsize = "medium" placeholder="Senha" type="password" register={register("password")}/>
                     <Button buttonsize="medium" buttonstyle="noneoutline">Entrar</Button> 
                 </form>
                 <section className="loginFormFooter">
