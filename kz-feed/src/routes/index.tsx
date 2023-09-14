31b4f0b607487ce731440bdfa85acdbd748229c1
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-13 21:57:43 -0300

    feat/RegisterPage route applied

--- kz-feed/src/routes/index.tsx
+++ kz-feed/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes } from "react-router"
 import { LoginPage } from "../pages/loginPage"
 import { Route } from "react-router-dom"
+import { RegisterPage } from "../pages/registerPage"
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path = "/login" element = {<LoginPage/>} />
+            <Route path = "/register" element={<RegisterPage/>} />
         </Routes>
     )
 }
