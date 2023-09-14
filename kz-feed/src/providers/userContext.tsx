0ad34ba06302cc83b8957589bdd43b66b0792792
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-14 20:16:05 -0300

    feat/UserContext getPosts logic

--- kz-feed/src/providers/userContext.tsx
+++ kz-feed/src/providers/userContext.tsx
@@ -1,15 +1,38 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { ReactNode } from 'react';
+import { api } from '../services/api';
 
 interface IUserProviderProps{
     children: ReactNode;
 }
 
+interface IPosts{
+    title: string;
+    description: string;
+    owner: string;
+    userId: number;
+    id: number;
+    image: string;
+}
+
 export const UserContext = createContext({})
 
 export const UserProvider = ({children}: IUserProviderProps) => {
+    const [posts, setPosts] = useState<IPosts[]>([])
 
-    
+    useEffect(() => {
+        const getPosts = async () => {
+            try{
+                const {data} = await api.get("/posts")
+                setPosts(data)
+            } catch (error){
+                console.log(error)
+            }
+        }
+
+        getPosts()
+    }, [])
+
     return(
         <UserContext.Provider value={{}}>
             {children}
