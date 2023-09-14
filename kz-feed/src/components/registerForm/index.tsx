02ddcd02c4b39d9490004dbd4c086b9252a3b3f0
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-13 23:51:11 -0300

    feat/RegisterAPI api applied

--- kz-feed/src/components/registerForm/index.tsx
+++ kz-feed/src/components/registerForm/index.tsx
@@ -4,7 +4,10 @@ import { Input } from "../input"
 import { ParagraphOne, TitleTwo } from "../typography"
 import { IRegisterFormValues, schema } from "./registerSchema";
 import { StyledRegisterFormContainer } from "./style"
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'
+import { useContext } from "react"
+import { SessionContext } from "../../providers/sessionContext";
+
 
 export interface IRegisterFormData{
     name: string;
@@ -18,8 +21,11 @@ export const RegisterForm = () => {
         resolver: zodResolver(schema)
     })
 
+    const {handleRegister} = useContext(SessionContext)
+
     const submit = async (formData: IRegisterFormData) => {
         console.log(formData)
+        await handleRegister(formData)
     }
     return(
         <StyledRegisterFormContainer>
